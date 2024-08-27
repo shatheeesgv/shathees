6ca1b70e075d25cc82e871ec3d70a160c8a228da
shatheeesgv <gvshathees@gmail.com>
2024-08-27 11:45:34 +0530

    Add files via upload

---
+++ pos.js
@@ -0,0 +1,16 @@
+const http=require('http');
+const path=require('path');
+const PORT=5000;
+const server=http.createServer((req,res)=>{
+    res.write("hi guys!");
+    res.end();
+});
+server.listen(PORT,(err)=>{
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log("server running in "+PORT);
+    }
+})
+console.log(path.dirname(__filename));
